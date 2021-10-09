088c1fb94bf6630fbfe6b906dfc0524251bcf943
Noudea <oudea.nicolas@gmail.com>
2021-10-09 12:53:08 +0200

    :heavy_minus_sign: removed use-chrome-storage
    does not work

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,27 +1,40 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-
 const Popup = () => {
 
-    return(<>
-        <style jsx>{`
-            
-            .popUpContainer {
-                width:300px;
-                height:500px;
-                border-radius:50px;
-            }
-            `}</style>        
-        
-        <div className='popUpContainer'>
-            
+
+    const setStoreKey = ({key : value}) => {
+        chrome.storage.sync.set({key : value}, function () {
+            console.log('Value is set to ' + value)
+        })
+    }
 
 
-        </div>
-    </>)
+    const getStoreKey = () => {
+        chrome.storage.sync.get(['key'], function (result) {
+            console.log('Value currently is ' + result.key)
+        })
+    }
 
+
+
+    return (
+        <>
+            <style jsx>{`
+                .popUpContainer {
+                    width: 300px;
+                    height: 500px;
+                    border-radius: 50px;
+                }
+            `}</style>
+
+            <div className="popUpContainer"></div>
+            <button onClick={()=> {
+                setStoreKey({test : 'mon test'})
+            }}>test</button>
+        </>
+    )
 }
 
-
-render(<Popup/>,document.getElementById('react-target'))
+render(<Popup />, document.getElementById('react-target'))
