6834f4997a1581a14e7d0ea92823adfa46ba7e9b
Noudea <oudea.nicolas@gmail.com>
2021-10-09 12:33:40 +0200

    :heavy_plus_sign: added use-chrome-storage
    added use-chrome-storage, react hook for using chrome storage

--- src/popup.jsx
+++ src/popup.jsx
@@ -5,7 +5,20 @@ import { render } from 'react-dom'
 const Popup = () => {
 
     return(<>
-        <p>DummyTexte</p>
+        <style jsx>{`
+            
+            .popUpContainer {
+                width:300px;
+                height:500px;
+                border-radius:50px;
+            }
+            `}</style>        
+        
+        <div className='popUpContainer'>
+            
+
+
+        </div>
     </>)
 
 }
